5c75474ad66d3c7564f2ffc61da44a78a9b9b360
gabrielSBorges <borgesgabrielsilva@gmail.com>
2020-12-03 20:48:13 -0300

    Layout base de relatórios OK

---
+++ cherry/WebContent/mixins/relatorios/lucro_diario.js
@@ -0,0 +1,7 @@
+export default {
+  headers: [
+    { title: 'Data', value: 'data', sortable: false },
+    { title: 'Quantidade de Vendas', value: 'qtd_vendas', sortable: false },
+    { title: 'Total', value: 'total', sortable: false },
+  ]
+}
---
+++ cherry/WebContent/mixins/relatorios/lucro_mensal.js
@@ -0,0 +1,8 @@
+export default {
+  
+  headers: [
+    { title: 'Dia', value: '', sortable: false },
+    { title: 'Valor', value: '', sortable: false },
+    { title: 'Dia', value: '', sortable: false },
+  ]
+}
